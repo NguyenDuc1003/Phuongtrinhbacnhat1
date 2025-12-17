b556343b92749f81d35e3cfd9373265fe160c2ce
NguyenDuc1003 <20223502@eaut.edu.vn>
2025-12-17 13:38:03 +0700

    up

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Giải phương trình bậc nhất ax + b = 0");
+        Console.Write("Nhập a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Nhập b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phương trình có vô số nghiệm.");
+            }
+            else
+            {
+                Console.WriteLine("Phương trình vô nghiệm.");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"Nghiệm của phương trình là x = {x}");
+        }
+    }
+}
